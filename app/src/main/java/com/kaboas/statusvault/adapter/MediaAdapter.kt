94a7b60b127adfd85ebe4dfdc3cf2f8f012114b6
Malek-Ahmed1 <malekahmed9572@gmail.com>
2026-09-15 19:48:28 +0000

    Use colorful SVG icons instead of emoji

--- app/src/main/java/com/kaboas/statusvault/adapter/MediaAdapter.kt
+++ app/src/main/java/com/kaboas/statusvault/adapter/MediaAdapter.kt
@@ -7,6 +7,7 @@ import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.google.android.material.button.MaterialButton
 import com.kaboas.statusvault.R
 import java.io.File
 
@@ -21,7 +22,7 @@ class MediaAdapter(
     inner class VH(v: View) : RecyclerView.ViewHolder(v) {
         val img: ImageView = v.findViewById(R.id.imgThumb)
         val playIcon: ImageView = v.findViewById(R.id.imgPlayIcon)
-        val btnDownload: ImageButton = v.findViewById(R.id.btnDownload)
+        val btnDownload: MaterialButton = v.findViewById(R.id.btnDownload)
         val btnFav: ImageButton = v.findViewById(R.id.btnFavorite)
         val btnDelete: ImageButton = v.findViewById(R.id.btnDelete)
     }
